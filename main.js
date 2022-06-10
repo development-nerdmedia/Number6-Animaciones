a17d313ee3c37ed83a63885f76ed514021ed0a77
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-06-10 09:42:29 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var animation4 = bodymovin.loadAnimation({
+var animation1 = bodymovin.loadAnimation({
     container: document.getElementById('A1'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Banner home.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation2 = bodymovin.loadAnimation({
     container: document.getElementById('A2'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Banner-nosotros.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation3 = bodymovin.loadAnimation({
     container: document.getElementById('A3'),
     rederer: 'svg',
     loop: true,
@@ -26,84 +26,84 @@ var animation4 = bodymovin.loadAnimation({
     autoplay: true,
     path: 'json/Mision.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation5 = bodymovin.loadAnimation({
     container: document.getElementById('A5'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Servicio1 v2.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation6 = bodymovin.loadAnimation({
     container: document.getElementById('A6'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'JSON/Servicio1.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation7 = bodymovin.loadAnimation({
     container: document.getElementById('A7'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'JSON/Servicio2.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation8 = bodymovin.loadAnimation({
     container: document.getElementById('A8'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'JSON/Servicio3.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation9 = bodymovin.loadAnimation({
     container: document.getElementById('A9'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'JSON/Servicio4.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation10 = bodymovin.loadAnimation({
     container: document.getElementById('A10'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'JSON/Servicio5.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation11 = bodymovin.loadAnimation({
     container: document.getElementById('A11'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Socios.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation12 = bodymovin.loadAnimation({
     container: document.getElementById('A12'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Valor1.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation13 = bodymovin.loadAnimation({
     container: document.getElementById('A13'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Valor2.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation14 = bodymovin.loadAnimation({
     container: document.getElementById('A14'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Valor3.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation15 = bodymovin.loadAnimation({
     container: document.getElementById('A15'),
     rederer: 'svg',
     loop: true,
     autoplay: true,
     path: 'json/Valor4.json'
 })
-var animation4 = bodymovin.loadAnimation({
+var animation16= bodymovin.loadAnimation({
     container: document.getElementById('A16'),
     rederer: 'svg',
     loop: true,
